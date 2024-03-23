5313a823a5650410451dae22105c23092db5a787
TimWong <timwong.tw93@gmail.com>
2024-03-23 14:03:03 +1100

    added back button for blog site and site can now store inputs into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
 const usernameInput = document.querySelector(`#username`);
 const titleInput = document.querySelector(`#blog-title`);
 const contentInput = document.querySelector(`#blog-content`);
+const form = document.querySelector(`#input-form`);
+const submitButton = document.querySelector(`#submit-button`);
+
+let blogInfo = [];
+
+function init() {
+
+  const storedInfo = JSON.parse(localStorage.getItem(`blogInfo`));
+  
+  if(storeInfo != null) {
+    blogInfo = storedInfo;
+  }
+}
+
+// function storeInfo() {
+
+//   localStorage.setItem(`blogInfo`, JSON.stringify(blogInfo));
+// }
+
+submitButton.addEventListener(`click`, function(event) {
+
+  let recentInfo = {
+    username: usernameInput.value,
+    title: titleInput.value,
+    content: contentInput.value,
+  };
+
+  // //checking if any inputs were not entered
+  // if(usernameInput === `` || titleInput === `` || contentInput === ``) {
+  //   return;
+  // }
+
+  // storeInfo();
+  localStorage.setItem(`blogInfo`, JSON.stringify(recentInfo));
+});
+
